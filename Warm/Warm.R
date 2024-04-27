ab2c0d05e7822d0ef480e03282435feb8dbfb297
Songporworn Mahapiyanont <songporwornmahapiyanont@gmail.com>
2024-04-27 15:31:45 +0700

    Revert "R-code"
    This reverts commit 30d838b82f4866d37c722e650dec961af38be23a.

--- Warm/Warm.R
+++
@@ -1,6 +0,0 @@
-y<-c(12,10,9,9,15,12,13,12,6,11,10,7)
-x<-c(1,1,1,1,2,2,2,2,3,3,3,3)
-x_factor<-factor(x)
-
-result_aov<-aov(y~x_factor)
-summary(result_aov)
--- absurb/Lab1 - create vector sentens.R
+++
@@ -1,8 +0,0 @@
-text.1 <- c("su","science")
-text.1
-text.2 <- c("Have a nice day")
-text.2
-
-
-text.3 <- c("Songporworn","Mahapiyanot")
-text.3
